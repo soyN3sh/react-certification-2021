1ccd7c99d01fa2df3a55cf06cd1ffab0b16ce3fa
Alejandro Huerta <alejandro.huerta@Alejandros-MacBook-Pro.local>
2021-10-15 16:39:05 -0500

    Removes unused components from App

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -3,10 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import AuthProvider from '../../providers/Auth';
 import HomePage from '../../pages/Home';
-import LoginPage from '../../pages/Login';
 import NotFound from '../../pages/NotFound';
-import SecretPage from '../../pages/Secret';
-import Private from '../Private';
 import Layout from '../Layout';
 import { random } from '../../utils/fns';
 
@@ -37,12 +34,6 @@ function App() {
             <Route exact path="/">
               <HomePage />
             </Route>
-            <Route exact path="/login">
-              <LoginPage />
-            </Route>
-            <Private exact path="/secret">
-              <SecretPage />
-            </Private>
             <Route path="*">
               <NotFound />
             </Route>
